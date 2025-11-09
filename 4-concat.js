1c2f07a9d98aad33800de7c3d44701df7bbad604
Hildah 365 <hildah.artspiration365@gmail.com>
2025-11-09 20:27:00 +0300

    Add 4-concat.js to print two arguments in 'arg1 is arg2' format

---
+++ 4-concat.js
@@ -0,0 +1,7 @@
+// Get the first and second arguments passed by the user
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+// Print them in the format "arg1 is arg2"
+console.log(`${arg1} is ${arg2}`);
+
